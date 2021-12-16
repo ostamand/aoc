bd25fa781e36d76671fad158d15bd0d411c5c93d
O1SA <olivier.st.amand.1@gmail.com>
2021-12-15 19:36:02 -0500

    Add solution for day 6 part#2

--- d6/d6.go
+++ d6/d6.go
@@ -28,6 +28,14 @@ func getData(path string) []int {
 	return data
 }
 
+func sumSlice(slice []int) int {
+	sum := 0
+	for _, v := range slice {
+		sum += v
+	}
+	return sum
+}
+
 func Solve(path string, part int) {
 
 	var nDays int
@@ -40,20 +48,24 @@ func Solve(path string, part int) {
 
 	lifes := getData(path)
 
-	for n := 1; n <= nDays; n++ {
-		nToAdd := 0
-		for idx, value := range lifes {
-			if value == 0 {
-				nToAdd++
-				lifes[idx] = 6
-			} else {
-				lifes[idx] = value - 1
-			}
-		}
-		for i := 0; i < nToAdd; i++ {
-			lifes = append(lifes, 8)
-		}
-		//fmt.Printf("After %2d days: %v\n", n, lifes)
+	count := make([]int, 9)
+	for _, l := range lifes {
+		count[l]++
 	}
-	fmt.Printf("Number of fish at the end: %d\n", len(lifes))
+
+	for n := 1; n <= nDays; n++ {
+		prev := make([]int, 9)
+		copy(prev, count)
+
+		// move left
+		for i := 0; i < 8; i++ {
+			count[i] = prev[i+1]
+		}
+		// 0 becomes a 6 and adds a new 8
+		count[6] += prev[0]
+		count[8] = prev[0]
+
+		//fmt.Printf("After %2d days: %v\n", n, count)
+	}
+	fmt.Printf("Number of fish at the end: %d\n", sumSlice(count))
 }
