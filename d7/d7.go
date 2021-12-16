5574d30468df6d3fb59d373b0cc4cc21f4f4df5d
O1SA <olivier.st.amand.1@gmail.com>
2021-12-15 20:17:18 -0500

    Add solution for day 7 part#2

--- d7/d7.go
+++ d7/d7.go
@@ -8,25 +8,28 @@ import (
 )
 
 func Solve(path string, part int) {
+	positions := helpers.ReadInts(path)
 
-	switch part {
-	case 1:
-		positions := helpers.ReadInts(path)
+	n := helpers.MinInts(positions)
+	m := helpers.MaxInts(positions)
 
-		n := helpers.MinInts(positions)
-		m := helpers.MaxInts(positions)
+	leastFuel := math.MaxInt
+	for i := n; i <= m; i++ {
+		fuel := 0
+		for _, p := range positions {
 
-		leastFuel := math.MaxInt
-		for i := n; i <= m; i++ {
-			fuel := 0
-			for _, p := range positions {
-				fuel += helpers.AbsInt(p - i)
-			}
-
-			if leastFuel > fuel {
-				leastFuel = fuel
+			distance := helpers.AbsInt(p - i)
+			if part == 1 {
+				fuel += distance
+			} else if part == 2 {
+				for i := 1; i <= distance; i++ {
+					fuel += i
+				}
 			}
 		}
-		fmt.Printf("Minimum fuel to align: %d\n", leastFuel)
+		if leastFuel > fuel {
+			leastFuel = fuel
+		}
 	}
+	fmt.Printf("Minimum fuel to align: %d\n", leastFuel)
 }
