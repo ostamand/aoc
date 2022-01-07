9bd84b0cb172016abb20500a8e642cd17be4b0b9
O1SA <olivier.st.amand.1@gmail.com>
2022-01-06 22:28:21 -0500

    Apply linting

--- 2021/src/js/d1/d1.js
+++ 2021/src/js/d1/d1.js
@@ -3,44 +3,44 @@ const select = document.querySelector("select.input")
 
 document.getElementById("part-1").addEventListener("click", () => {
     getData()
-    .then(data => {
-        let nIncreases = 0
-        for(let i=1; i<data.length; i++){
-            if (data[i] > data[i-1]){
-                nIncreases++
+        .then(data => {
+            let nIncreases = 0
+            for (let i = 1; i < data.length; i++) {
+                if (data[i] > data[i - 1]) {
+                    nIncreases++
+                }
             }
-        }
-        output.textContent = `Measurements larger: ${nIncreases}`
-    })
+            output.textContent = `Measurements larger: ${nIncreases}`
+        })
 })
 
 document.getElementById("part-2").addEventListener("click", () => {
     getData()
-    .then(data => {
-        let sums = []
-        for(let i=0; i<data.length - 2; i++){
-            sums.push(data.slice(i, i+3).reduce((total, current) => {
-                return total + current
-            }, 0))
-        }
-
-        let nIncreases = 0
-        for(let i=1; i < sums.length; i++){
-            if (sums[i] > sums[i-1]){
-                nIncreases++
+        .then(data => {
+            let sums = []
+            for (let i = 0; i < data.length - 2; i++) {
+                sums.push(data.slice(i, i + 3).reduce((total, current) => {
+                    return total + current
+                }, 0))
             }
-        }
 
-        output.textContent = `Sums larger: ${nIncreases}`
-    })
+            let nIncreases = 0
+            for (let i = 1; i < sums.length; i++) {
+                if (sums[i] > sums[i - 1]) {
+                    nIncreases++
+                }
+            }
+
+            output.textContent = `Sums larger: ${nIncreases}`
+        })
 })
 
 
 function getData() {
     const filePath = `../../../inputs/${select.options[select.selectedIndex].text}`
     return fetch(filePath)
-    .then(response => response.text())
-    .then(text => processText(text))
+        .then(response => response.text())
+        .then(text => processText(text))
 }
 
 /**
